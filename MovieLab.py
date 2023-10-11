d2d0a6cdcab2b6fe0ebde0490ea690fbbe31dcf4
Reef <reef.zayed2@gmail.com>
2023-10-11 14:28:46 +0300

    Sol' Done

---
+++ MovieLab.py
@@ -0,0 +1,14 @@
+
+movieName ="Suits"
+ratingMovie:int =3
+popularityScore:float =72.65
+
+if ratingMovie>=4 and popularityScore>80:
+    print("Highly recommended")
+elif ratingMovie>=3 and popularityScore>70:
+    print ("I recommended it . It is good")
+elif ratingMovie<=2 and popularityScore>60: 
+    print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
+
