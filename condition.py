984c42ff7c76f9d5dd7a752621bd97551ce2cdad
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-10-11 14:16:58 +0300

    Solution

---
+++ condition.py
@@ -0,0 +1,12 @@
+movie_name = "Kings"
+kings_moive_rate = 3
+kings_popularity_score = 72.65
+
+if kings_moive_rate == 4 and kings_popularity_score > 80:
+    print("Highly recommended")
+elif kings_moive_rate == 3 and kings_popularity_score > 70:
+    print("I recommended it . It is good")
+elif kings_moive_rate == 2 and kings_popularity_score > 60:
+    print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
